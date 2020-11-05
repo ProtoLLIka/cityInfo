b418618bdf5ed9bcb4e5052ea0e8ac01216637b5
Anton Sapunkov <49634186+ProtoLLIka@users.noreply.github.com>
2020-11-05 17:06:45 +0400

    Updates

---
+++ precache-manifest.54a07f6f85796baa2293018ada87ecc8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bcf71d5ff724eff3664add6b8277d9",
+    "url": "/cityInfo/index.html"
+  },
+  {
+    "revision": "526f1736a835f720a9ae",
+    "url": "/cityInfo/static/js/2.f336d02f.chunk.js"
+  },
+  {
+    "revision": "ec4f719a23ac253338fbdd40cd5e0c66",
+    "url": "/cityInfo/static/js/2.f336d02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50256860215c148c82f3",
+    "url": "/cityInfo/static/js/main.a61d65fe.chunk.js"
+  },
+  {
+    "revision": "7721c8209a4c8fc2f14c",
+    "url": "/cityInfo/static/js/runtime-main.098a7657.js"
+  },
+  {
+    "revision": "c229caf7779482c01a82a40d7c5930a0",
+    "url": "/cityInfo/static/media/artGalary.c229caf7.jpg"
+  },
+  {
+    "revision": "24715a8e6766c98707c35e19ce92d884",
+    "url": "/cityInfo/static/media/cinema.24715a8e.jpg"
+  },
+  {
+    "revision": "e25a4a6e52a4977eddec1a3dd44f8698",
+    "url": "/cityInfo/static/media/comedyClub.e25a4a6e.jpg"
+  },
+  {
+    "revision": "9f7a336a2685405c42f8fc6d0c5e7425",
+    "url": "/cityInfo/static/media/concertVenue.9f7a336a.jpg"
+  },
+  {
+    "revision": "236d8ac75b967f9e62ccbaaa397307f3",
+    "url": "/cityInfo/static/media/fire.236d8ac7.svg"
+  },
+  {
+    "revision": "2b680f81e6d2541235f43eb25c500f48",
+    "url": "/cityInfo/static/media/historicalSite.2b680f81.jpg"
+  },
+  {
+    "revision": "82161d97387be879bab5334096ebb2ca",
+    "url": "/cityInfo/static/media/logo.82161d97.png"
+  },
+  {
+    "revision": "502662d54b8fdadee9439fab6a5cff8f",
+    "url": "/cityInfo/static/media/museum.502662d5.jpg"
+  },
+  {
+    "revision": "91933c8fba6b8ba3d92ab590008abeb3",
+    "url": "/cityInfo/static/media/radiation.91933c8f.svg"
+  },
+  {
+    "revision": "a7d605497191d6a9b4d6a732bfb3b640",
+    "url": "/cityInfo/static/media/snowflake.a7d60549.svg"
+  },
+  {
+    "revision": "526114643f959d97d53929a429d4af3c",
+    "url": "/cityInfo/static/media/sun.52611464.svg"
+  }
+]);
